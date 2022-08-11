f1cd808c23dc72bab945974c59da872b8ffabfc8
sungyotkim <sungyotkim@gmail.com>
2022-08-10 22:07:02 -0400

    set up display class

---
+++ display.rb
@@ -0,0 +1,9 @@
+require_relative "board.rb"
+require_relative "cursor.rb"
+
+class Display
+    def initialize(board)
+        @board = board
+        @cursor = Cursor.new([0,0], board)
+    end
+end
