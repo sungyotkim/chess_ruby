384ac8bc3d45b85d1f9f3648b796e6cdb0900630
sungyotkim <sungyotkim@gmail.com>
2022-08-10 21:59:33 -0400

    add pawn movement

--- pieces/pawn.rb
+++ pieces/pawn.rb
@@ -26,4 +26,63 @@ class Pawn < Piece
             return [1, 0]
         end
     end
+
+    def moves
+        arr = []
+        if self.at_start_row?
+            arr += start_move if !start_move.nil?
+            arr += side_attacks if !side_attacks.nil? #can attack even if front is blocked
+        else
+            p arr
+            arr << forward_steps if !forward_steps.nil? #this returns one array so we push it
+            arr += side_attacks if !side_attacks.nil? #can attack even if front is blocked
+        end
+        arr
+    end
+
+    def start_move
+        arr = []
+        start_pos = self.pos
+        row, col = start_pos
+        dx, dy = forward_dir
+
+        forward_one = [row + dx, col + dy]
+        forward_two = [row + dx + dx, col + dy + dy] #pawns can move 2 spaces if at the starting position
+
+        if @board[forward_one[0]][forward_one[1]].is_a?(NullPiece) 
+            arr << forward_one
+        end
+        
+        if @board[forward_two[0]][forward_two[1]].is_a?(NullPiece) && @board[forward_one[0]][forward_one[1]].is_a?(NullPiece) #can only move if front two spots are empty
+            arr << forward_two
+        end
+        arr
+    end
+
+    def forward_steps
+        start_pos = self.pos
+        row, col = start_pos
+        dx, dy = forward_dir
+
+        if row + dx >= 0 && row + dx < 8
+            if @board[row + dx][col].is_a?(NullPiece) #can only move if empty spot in front
+                return [row + dx, col]
+            else
+                return []
+            end
+        end
+    end
+
+    def side_attacks
+        start_pos = self.pos
+        current_color = self.color
+        row, col = start_pos
+        arr = []
+        side_dir.each do |dx, dy|
+            if !@board[row + dx][col + dy].is_a?(NullPiece) && @board[row + dx][col + dy].color != current_color #check to see if side attack is possible
+                arr << [row + dx, col + dy]
+            end
+        end
+        arr
+    end
 end
