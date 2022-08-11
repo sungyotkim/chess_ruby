e731e44c58b81706d353054aac088fc7dba92850
sungyotkim <sungyotkim@gmail.com>
2022-08-10 21:49:20 -0400

    add movement array for king

--- pieces/king.rb
+++ pieces/king.rb
@@ -1,6 +1,8 @@
 require "./piece.rb"
 
 class King < Piece
+    include Steppable
+
     def initialize(color, board, pos)
         super(color, board, pos)
         color == :black ? @symbol = "♚" : @symbol = "♔"
@@ -8,5 +10,18 @@ class King < Piece
 
     def to_s
         @symbol
+    end
+
+    def move_diffs
+        [
+            [0, -1],
+            [-1, -1],
+            [-1, 0],
+            [-1, 1],
+            [0, 1],
+            [1, 1],
+            [1, 0],
+            [1, -1]
+        ]
     end
 end
