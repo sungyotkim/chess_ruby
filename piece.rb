d5f6638e44743932319118621af8eeda9b004b77
sungyotkim <sungyotkim@gmail.com>
2022-08-10 21:19:50 -0400

    set up piece methods

--- piece.rb
+++ piece.rb
@@ -1,5 +1,8 @@
 class Piece
-    def initialize
+    def initialize(color, board, pos)
+        @color = color
+        @board = board
+        @pos = pos
         @symbol = "♟"
     end
 
@@ -7,4 +10,14 @@ class Piece
         @symbol
     end
 
+    def empty?(pos)
+        @board[pos[0]][pos[1]].is_a?(NullPiece)
+    end
+
+    def pos=(val)
+        @pos = val
+    end
+
+    private
+    attr_reader :color, :pos
 end
