3e46e15603061cfd979ad24b76dd78fc5e40bd6a
sungyotkim <sungyotkim@gmail.com>
2022-08-10 21:48:46 -0400

    add movement array for knight

--- pieces/knight.rb
+++ pieces/knight.rb
@@ -1,6 +1,8 @@
 require "./piece.rb"
 
 class Knight < Piece
+    include Steppable
+
     def initialize(color, board, pos)
         super(color, board, pos)
         color == :black ? @symbol = "♞" : @symbol = "♘"
@@ -8,5 +10,18 @@ class Knight < Piece
 
     def to_s
         @symbol
+    end
+
+    def move_diffs #array of 8 possible movements
+        [
+            [-2, -1],
+            [-2,  1],
+            [-1, -2],
+            [-1,  2],
+            [ 1, -2],
+            [ 1,  2],
+            [ 2, -1],
+            [ 2,  1]
+        ]
     end
 end
