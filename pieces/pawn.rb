05171c1c4b0fd1ac99a47b31101d815c72845361
sungyotkim <sungyotkim@gmail.com>
2022-08-10 21:55:10 -0400

    check to see if pawn is at start

--- pieces/pawn.rb
+++ pieces/pawn.rb
@@ -9,4 +9,13 @@ class Pawn < Piece
     def to_s
         @symbol
     end
+
+    def at_start_row?
+        if self.color == :white
+            return true if self.pos[0] == 6
+        else
+            return true if self.pos[0] == 1
+        end
+        false
+    end
 end
