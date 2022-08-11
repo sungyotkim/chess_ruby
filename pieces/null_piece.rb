cdb1f6665c76d6f8d5706bcaadb97f78e2a9f4a8
sungyotkim <sungyotkim@gmail.com>
2022-08-10 21:16:25 -0400

    set up null piece class

---
+++ pieces/null_piece.rb
@@ -0,0 +1,14 @@
+require "/piece"
+require "singleton"
+
+class NullPiece < Piece
+    include Singleton
+    
+    def initialize
+        @symbol = ' '
+    end
+
+    def to_s
+        @symbol
+    end
+end
