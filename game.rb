249f2c6e29700796f793e9213167a644d7184fc8
sungyotkim <sungyotkim@gmail.com>
2022-08-12 10:01:14 -0400

    set up initialize

--- game.rb
+++ game.rb
@@ -2,6 +2,12 @@ require_relative 'board.rb'
 
 def Game
   def initialize
-
+    @board = Board.new
+    @display = Display.new(@board)
+    @players = {
+      white: 'Player1',
+      black: 'Player2'
+    }
+    @current_player = :white
   end
 end
